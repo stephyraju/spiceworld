5e3a765de31e8b825889d55fd6f6f5cd5976b30a
Ubuntu <ubuntu@ip-172-31-92-35.ec2.internal>
2019-08-25 10:47:38 +0000

    Created base.html

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'cook_book'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
-
+mongo = PyMongo(app)
 @app.route('/')
 def hello():
     return 'Hello World ...again'
