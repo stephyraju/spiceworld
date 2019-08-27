547acaadc66357f0ca26e779e41875e466c3cebd
Ubuntu <ubuntu@ip-172-31-92-35.ec2.internal>
2019-08-27 06:15:49 +0000

    Add addrecipe.html file

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ mongo = PyMongo(app)
 @app.route('/get_recipes')
 def get_recipes():
      return render_template("index.html", recipes=mongo.db.recipes.find())
-
+     
+@app.route('/add_recipe')
+def add_recipe():
+     return render_template("addrecipe.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
