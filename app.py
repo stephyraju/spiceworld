0e180cca2f4780f2f6efaa6192390a7bf050d0eb
Ubuntu <ubuntu@ip-172-31-92-35.ec2.internal>
2019-08-29 05:34:18 +0000

    Added Materialize Form options to categories

--- app.py
+++ app.py
@@ -16,8 +16,15 @@ def get_recipes():
      
 @app.route('/add_recipe')
 def add_recipe():
-     return render_template("addrecipe.html")
+     return render_template("addrecipe.html",  
+               categories=mongo.db.categories.find())
 
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
