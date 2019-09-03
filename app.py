876872f5f0c845a66cceacccc213aa72303eddf4
Ubuntu <ubuntu@ip-172-31-92-35.ec2.internal>
2019-09-03 17:06:03 +0000

    Add recipe functions added

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+# declaring app name
 app = Flask(__name__)
+
 app.config["MONGO_DBNAME"] = 'cook_book'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
@@ -17,21 +19,27 @@ def get_recipes():
 @app.route('/add_recipe')
 def add_recipe():
      return render_template("addrecipe.html",  
-               categories=mongo.db.categories.find())
+               recipes=mongo.db.recipes.find(),
+               categories = mongo.db.categories.find(), 
+               cuisines=mongo.db.cuisines.find(), 
+               difficulty=mongo.db.difficulty.find(), 
+               allergens=mongo.db.allergens.find())
+               
+ 
+@app.route('/insert_recipe', methods=['GET', 'POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))              
                
 @app.route('/view_recipes', methods=['POST', 'GET'])
-
 def view_recipes():
      return render_template("recipes.html",  
                categories=mongo.db.categories.find())
 
 
 
-@app.route('/insert_recipe', methods=['POST'])
-def insert_recipe():
-    recipes = mongo.db.recipes
-    recipes.insert_one(request.form.to_dict())
-    return redirect(url_for('get_recipes'))
+
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
--- static/js/main.js
+++ static/js/main.js
@@ -4,4 +4,26 @@ $(document).ready(function(){
     $('select').formSelect();
     $('.carousel').carousel();
  
-  });
+  });    
+ 
+ var ingredientField = $(".ingredient").length;
+    $("#add_ingredient").on("click", function () {
+        $("select").formSelect("destroy");
+        $(".new-ingredient:first").clone().insertBefore("#add_ingredient").find("input[type='text'], select, textarea").val("");
+        $("select").formSelect();
+        ingredientField += 1;
+    });
+
+    $("#remove_ingredient").on("click", function () {
+        if (ingredientField > 1) {
+            /* only remove the :last item */
+            $(this).siblings(".new-ingredient:last").remove();
+            /* ensure original ingredient line never gets deleted */
+            ingredientField-= 1;
+        }
+    });
+ 
+ 
+ 
+    
+
